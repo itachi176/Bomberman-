1a321ecbb8db6a1e5e074979fc6b01eeea17dd50
DucAnh <daoducanhc@gmail.com>
2020-12-10 15:43:26 +0700

    clean code

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -2,13 +2,11 @@ package uet.oop.bomberman;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import uet.oop.bomberman.bomb.Bomb;
 import uet.oop.bomberman.bomb.Flame;
